290cbed4e354d32918e5d3b0f5203a950dbab11f
Alexander Corr <xandrcorr@gmail.com>
2019-04-20 09:27:35 +0500

    More tests.

--- src/test_repo.py
+++ src/test_repo.py
@@ -1,4 +1,7 @@
 import unittest
+import time
+
+from pymongo.errors import DuplicateKeyError
 
 from infrastructure.repository import Repository
 from infrastructure.parser import Parser
@@ -8,14 +11,21 @@ from infrastructure.errors import BadArgumentError
 class TestRepository(unittest.TestCase):
 
     def setUp(self):
+
         with open('resources/example.html', 'r') as f_in:
             html = f_in.read()
-        self.repo = Repository()
+        self.repo = Repository(database="test_db")
         self.repo.add_many(Parser.parse_news(html))
 
     def test_add(self):
-        # TODO: test exceptions
-        pass
+        good = [
+            {'title': 'Title1', 'url': 'http://example.com/1', 'created': time.time()},
+            {'title': 'Title2', 'url': 'http://example.com/2', 'created': time.time()}
+        ]
+        self.assertEqual(self.repo.add_many(good), 2)
+        self.assertEqual(self.repo.add_many([]), 0)
+        with self.assertRaises(DuplicateKeyError):
+            self.repo.add(good[0])
 
     def test_get(self):
         # Assuming that database if filled at least once
@@ -31,6 +41,8 @@ class TestRepository(unittest.TestCase):
         with self.assertRaises(BadArgumentError):
             self.repo.get(sort_key="")
 
+    def tearDown(self):
+        self.repo.clean_db()
 
 if __name__ == "__main__":
     unittest.main()
