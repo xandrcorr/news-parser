dd1afe877093fdd1a0f64a3f90365f4d0b7eef23
Alexander Corr <xandrcorr@gmail.com>
2019-04-20 10:18:34 +0500

    Added logging to main module.

--- src/main.py
+++ src/main.py
@@ -6,12 +6,15 @@ from datetime import datetime
 import json
 import os
 
+from infrastructure import LoggerFactory
 from infrastructure.parser import Parser
 from infrastructure.repository import Repository
 
 update_timeout = int(os.environ.get('POST_UPDATE_TIMEOUT', "3600"))
 routes = web.RouteTableDef()
 repo = Repository()
+common_logger = LoggerFactory.create_logger("Main")
+
 
 @routes.get('/')
 async def index_handler(request):
@@ -36,7 +39,7 @@ async def posts_handler(request):
                                          sort_key=sort_key,
                                          sort_order=sort_order)
         except:
-            print(traceback.format_exc())
+            common_logger.error(traceback.format_exc())
             # TODO: return error reason
             return web.Response(text="Bad request", status=400)
     return web.Response(text=json.dumps(output, indent=2), status=200)
@@ -53,14 +56,18 @@ async def fetch(session, url):
 
 async def main():
     while True:
-        async with aiohttp.ClientSession() as session:
-            raw_html = await fetch(session, 'https://news.ycombinator.com/news')
-            news = Parser.parse_news(raw_html)
-            added = repo.add_many(news)
-            t = datetime.utcnow().isoformat()
-            print("[{0}] Added {1} news".format(t, added))
-        # check for a new articles every 1 hour
-        await asyncio.sleep(update_timeout)
+        try:
+            common_logger.info("Trying to get new posts from https://news.ycombinator.com/news")
+            async with aiohttp.ClientSession() as session:
+                raw_html = await fetch(session, 'https://news.ycombinator.com/news')
+                news = Parser.parse_news(raw_html)
+                added = repo.add_many(news)
+                common_logger.info("Added {0} posts".format(added))
+        except:
+            common_logger.error(traceback.format_exc())
+        finally:
+            # check for a new articles every 1 hour
+            await asyncio.sleep(update_timeout)
 
 
 if __name__ == "__main__":
